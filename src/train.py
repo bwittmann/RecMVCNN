7ca8d07f13e34e147140ecb69a00f4825ac93b12
Bastian <wittmann_@gmx.de>
2021-06-18 17:38:43 +0200

    Added checkpoints.

--- src/train.py
+++ src/train.py
@@ -1,3 +1,4 @@
+import os
 
 import torch 
 import numpy as np
@@ -6,6 +7,7 @@ import torch.optim as optim
 
 from torch.utils.tensorboard import SummaryWriter
 from tqdm import tqdm
+from dotenv import dotenv_values
 
 
 def train(device, model, args, train_dataloader, val_dataloader):
@@ -33,81 +35,112 @@ def train(device, model, args, train_dataloader, val_dataloader):
     total_classification = 0
 
     model.train()
-    # Training loop
-    for epoch in range(args.epoch):
-        print('Starting epoch:', epoch)
-        for batch_idx, batch in enumerate(train_dataloader):
+    try:
+        # Training loop
+        for epoch in range(1, args.epoch + 1):
+            print('Starting epoch:', epoch)
+            for batch_idx, batch in enumerate(train_dataloader):
 
-            _, renderings, class_labels, voxels = batch
-            renderings, class_labels, voxels = renderings.to(device), class_labels.to(device), voxels.to(device)
+                _, renderings, class_labels, voxels = batch
+                renderings, class_labels, voxels = renderings.to(device), class_labels.to(device), voxels.to(device)
 
-            # Predict and estimate loss
-            predictions = model(renderings.float())
-            train_loss_classification = criterion_classification(predictions, class_labels)
+                # Predict and estimate loss
+                predictions = model(renderings.float())
+                train_loss_classification = criterion_classification(predictions, class_labels)
 
-            # Make a step
-            optimizer.zero_grad()
-            train_loss_classification.backward()
-            optimizer.step()
+                # Backprob and make a step
+                optimizer.zero_grad()
+                train_loss_classification.backward()
+                optimizer.step()
 
-            train_loss_running_classification += train_loss_classification.item()
-            iteration = epoch * len(train_dataloader) + batch_idx
+                train_loss_running_classification += train_loss_classification.item()
+                iteration = (epoch - 1) * len(train_dataloader) + batch_idx
 
-            # Print and log train loss and train acc
-            if iteration % args.verbose == 0:
-                # Logging 
-                print('[{}/{}] train_loss: {}'.format(epoch, iteration, train_loss_running_classification / args.verbose))
-                tb_logger.add_scalar('loss/train_cls', train_loss_running_classification / args.verbose, iteration)
-                tb_logger.add_scalar('epoch', epoch, iteration)
+                # Print and log train loss and train acc
+                if iteration % args.verbose == 0:
+                    # Logging 
+                    print('[{}/{}] train_loss: {}'.format(epoch, iteration, train_loss_running_classification / args.verbose))
+                    tb_logger.add_scalar('loss/train_cls', train_loss_running_classification / args.verbose, iteration)
+                    tb_logger.add_scalar('epoch', epoch, iteration)
 
-                train_loss_running_classification = 0.
+                    train_loss_running_classification = 0.
 
-            # Validate on validation set
-            if iteration % args.val_step == 0:
-                print('Starting validation')
-                model.eval()
+                # Validate on validation set
+                if iteration % args.val_step == 0 and not args.no_validation:
+                    print('Starting validation')
+                    model.eval()
 
-                for batch in tqdm(val_dataloader):
-                    _, renderings, class_labels, voxels = batch
-                    renderings, class_labels, voxels = renderings.to(device), class_labels.to(device), voxels.to(device)
+                    for batch in tqdm(val_dataloader):
+                        _, renderings, class_labels, voxels = batch
+                        renderings, class_labels, voxels = renderings.to(device), class_labels.to(device), voxels.to(device)
 
-                    with torch.no_grad():
-                        predictions = model(renderings.float())
-                        correct_pred = evaluate_classification(predictions, class_labels)
+                        with torch.no_grad():
+                            predictions = model(renderings.float())
+                            correct_pred = evaluate_classification(predictions, class_labels)
 
-                        val_loss_classification = criterion_classification(predictions, class_labels)
+                            val_loss_classification = criterion_classification(predictions, class_labels)
 
-                        # Update loss and acc related values
-                        val_loss_running_classification += val_loss_classification
-                        total_classification += predictions.shape[0]
-                        correct_classification += correct_pred 
+                            # Update loss and acc related values
+                            val_loss_running_classification += val_loss_classification
+                            total_classification += predictions.shape[0]
+                            correct_classification += correct_pred 
 
-                # Estimate val loss and acc
-                accuracy_classificaton = 100 * correct_classification / total_classification
-                val_loss_classification = val_loss_running_classification / len(val_dataloader)
+                    # Estimate val loss and acc
+                    accuracy_classificaton = 100 * correct_classification / total_classification
+                    val_loss_classification = val_loss_running_classification / len(val_dataloader)
 
-                # Save model is accuracy is better that all time best
-                if accuracy_classificaton >= best_accuracy_classification:
-                    print('best classification accuracy -> save model')
-                    best_accuracy_classification = accuracy_classificaton
-                    torch.save(model.state_dict(), 'exercise_2/outputs/{}/model_best.ckpt'.format(args.tag))
+                    # Make a step based on the validation loss
+                    scheduler.step(val_loss_classification)
 
-                # Logging 
-                print('[{}/{}] val_loss: {}, val_acc_cls: {}'.format(epoch, iteration, val_loss_classification, accuracy_classificaton))
-                tb_logger.add_scalar('loss/val_cls', val_loss_running_classification / len(val_dataloader), iteration)
-                tb_logger.add_scalar('acc/val_cls', accuracy_classificaton, iteration)
+                    # Save model is accuracy is better that all time best
+                    if accuracy_classificaton >= best_accuracy_classification:
+                        best_accuracy_classification = accuracy_classificaton
+                        print('best classification accuracy -> save model')
+                        save_model(model)
 
-                # Reset loss and acc related values
-                val_loss_running_classification = 0.
-                correct_classification = 0
-                total_classification = 0
+                    # Logging 
+                    print('[{}/{}] val_loss: {}, val_acc_cls: {}'.format(epoch, iteration, val_loss_classification, accuracy_classificaton))
+                    tb_logger.add_scalar('loss/val_cls', val_loss_running_classification / len(val_dataloader), iteration)
+                    tb_logger.add_scalar('acc/val_cls', accuracy_classificaton, iteration)
 
-                model.train()
+                    # Reset loss and acc related values
+                    val_loss_running_classification = 0.
+                    correct_classification = 0
+                    total_classification = 0
+
+                    model.train()
+
+        # Save checkpoint
+        print('training finished -> saving checkpoint')
+        save_model(model, epoch, optimizer, args, True)
+    except KeyboardInterrupt:
+        # Save checkpoint
+        print('keyboard interrupt -> saving checkpoint')
+        save_model(model, epoch, optimizer, args, True)
+
+
+def save_model(model, epoch=None, optimizer=None, args=None, checkpoint=False):
+    # TODO: test, not sure if scheduler also part of checkpoint
+    checkpoint_path = os.path.join(dotenv_values('.env')['MODEL_ROOT'], 'outputs/{}'.format(args.tag))
+    if not os.path.exists(checkpoint_path):
+        os.makedirs(checkpoint_path)
+
+    if checkpoint:
+        save_dict = {
+            'epoch': epoch, 
+            'model_state_dict' : model.state_dict(),
+            'optim_state_dict' : optimizer.state_dict() 
+        }
+        torch.save(save_dict, os.path.join(checkpoint_path, 'checkpoint.tar'))
+    else:
+        torch.save(model.state_dict(), os.path.join(checkpoint_path, 'model_best.pth'))
+
 
 def evaluate_classification(predictions, labels):
     pred_labels = torch.argmax(predictions, dim=1)
     correct_pred = (pred_labels == labels).sum().item()
     return correct_pred
+
 
 def evaluate_reconstruction():
     pass
