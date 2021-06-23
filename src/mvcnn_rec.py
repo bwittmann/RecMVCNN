93415064e977923305321524527fabc8e8365b78
Bastian <wittmann_@gmx.de>
2021-06-23 21:37:45 +0200

    Added a backbone with reduced feature size for testing. Otherwise the params of the model blow up for the reconstruction task. Already tested on overfit dataset with 100% acc.

--- src/mvcnn_rec.py
+++ src/mvcnn_rec.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn as nn
+from torch.nn.modules import padding
 import torchvision.models as models
 
 
@@ -36,6 +37,25 @@ class MVCNNRec(nn.Module):
             self.features = nn.Sequential(*list(mobnet.children())[:-2])
             in_features = 576*5*5
             in_channels = 1800
+        elif backbone == 'test':
+            resnet = models.resnet18(pretrained=True)
+            self.features = nn.Sequential(
+                *list(resnet.children())[:-4],
+                # Additional layers to reduce channels and spacial dim simulataneously ([64,4,4] vs [64,7,7])
+                nn.Conv2d(in_channels=128, out_channels=128, kernel_size=3, padding=1),
+                nn.BatchNorm2d(128),
+                nn.ReLU(),
+                nn.Conv2d(in_channels=128, out_channels=64, kernel_size=3, padding=1),
+                nn.BatchNorm2d(64),
+                nn.ReLU(),
+                nn.MaxPool2d(kernel_size=2),
+                nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, padding=1),
+                nn.BatchNorm2d(64),
+                nn.ReLU(),
+                nn.MaxPool2d(kernel_size=2),
+            )
+            in_features = 64*4*4
+            in_channels = 128
         else:
             raise NotImplementedError
 
@@ -44,7 +64,7 @@ class MVCNNRec(nn.Module):
             nn.Linear(in_features=in_features, out_features=4096, bias=True),
             nn.ReLU(inplace=True),
             nn.Dropout(p=0.5, inplace=False),
-            # TODO: think about cutting this layer 
+            # TODO: think about cutting this layer
             nn.Linear(in_features=4096, out_features=4096),
             nn.ReLU(inplace=True),
             nn.Dropout(p=0.5, inplace=False),
@@ -52,29 +72,29 @@ class MVCNNRec(nn.Module):
         )
 
         # Decoder for the reconstruction of 3D features
-        # TODO: think of adding bias, num channels too much?
+        # TODO: think of adding bias, num channels too much (vanilla version: 392 channels) -> network that reduces channels and spacial dim
         self.decoder_features = nn.Sequential(
-            # Layer 1: out [B, 256, 4, 4, 4]
-            nn.ConvTranspose3d(in_channels=in_channels, out_channels=256, kernel_size=4, stride=2, bias=False, padding=1),
-            nn.BatchNorm3d(256),
-            nn.ReLU(),
-            # Layer 2: out [B, 128, 8, 8, 8]
-            nn.ConvTranspose3d(in_channels=256, out_channels=128, kernel_size=4, stride=2, bias=False, padding=1),
+            # Layer 1: out [B, 128, 4, 4, 4]
+            nn.ConvTranspose3d(in_channels=in_channels, out_channels=128, kernel_size=4, stride=2, bias=False, padding=1),
             nn.BatchNorm3d(128),
             nn.ReLU(),
-            # Layer 3: out [B, 64, 16, 16, 16]
+            # Layer 2: out [B, 64, 8, 8, 8]
             nn.ConvTranspose3d(in_channels=128, out_channels=64, kernel_size=4, stride=2, bias=False, padding=1),
             nn.BatchNorm3d(64),
             nn.ReLU(),
-            # Layer 4: out [B, 32, 32, 32, 32]
+            # Layer 3: out [B, 32, 16, 16, 16]
             nn.ConvTranspose3d(in_channels=64, out_channels=32, kernel_size=4, stride=2, bias=False, padding=1),
             nn.BatchNorm3d(32),
+            nn.ReLU(),
+            # Layer 4: out [B, 32, 32, 32, 32]
+            nn.ConvTranspose3d(in_channels=32, out_channels=8, kernel_size=4, stride=2, bias=False, padding=1),
+            nn.BatchNorm3d(8),
             nn.ReLU(),
         )
 
         # Decoder for the reconstruction of 3D volumes from 3D features (1x1 conv)
         self.decoder_volume = nn.Sequential(
-            nn.ConvTranspose3d(in_channels=32, out_channels=1, kernel_size=1, bias=False),
+            nn.ConvTranspose3d(in_channels=8, out_channels=1, kernel_size=1, bias=False),
             nn.Sigmoid()
         )
 
@@ -86,14 +106,14 @@ class MVCNNRec(nn.Module):
 
         feature_list = []
         for view in x:
-            view_features = self.features(view) # [B, 512, 4, 4]
+            view_features = self.features(view) # see backbones for shape
             feature_list.append(view_features)
 
         # View pooling for classification results
         max_features = feature_list[0].view(view_features.shape[0], -1)
         for view_features in feature_list[0:]:
             view_features = view_features.view(view_features.shape[0], -1)
-            max_features = torch.max(max_features, view_features) # [B, 8192]
+            max_features = torch.max(max_features, view_features) # [B, in_features]
 
         # Get classificaton return
         cls_ret = self.classifier(max_features) # [B, num_classes]
@@ -104,12 +124,12 @@ class MVCNNRec(nn.Module):
         # Decoding of features for reconstruction
         for view_features in feature_list:
             view_features = view_features.view(batch_size, -1, 2, 2, 2) # [B, C, 2, 2, 2]
-            decoded_features = self.decoder_features(view_features) # [B, 32, 32, 32, 32]
+            decoded_features = self.decoder_features(view_features) # [B, 8, 32, 32, 32]
             raw_decoded_features = decoded_features
 
             generated_volume = self.decoder_volume(decoded_features) # [B, 1, 32, 32, 32]
 
-            raw_decoded_features = torch.cat((raw_decoded_features, generated_volume), dim=1) # [B, 33, 32, 32, 32]
+            raw_decoded_features = torch.cat((raw_decoded_features, generated_volume), dim=1) # [B, 9, 32, 32, 32]
 
             generated_volume_list.append(generated_volume.squeeze())
             raw_decoded_features_list.append(raw_decoded_features)
